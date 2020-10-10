0140e5e84b9406bd20a9de1b879ad9a7910684ba
sebastiantoad29 <53270020+sebastiantoad29@users.noreply.github.com>
2020-10-09 19:49:04 -0500

    Update Hammazon.java

--- src/practica1/Hammazon.java
+++ src/practica1/Hammazon.java
@@ -2,28 +2,31 @@ package myp.practica1;
 
 public class Hammazon extends Suscripcion{
 
+  static final int MEMBRESIA_N_HAMMAZON = 7;
+  static final int MEMBRESIA_P_HAMMAZON = 13;
+
   public void cobroHammazon(Object o, Cliente cliente, String tipoDeMembresia){
+
     if(!(o instanceof Hammazon))
       return;
     if(cliente.getSaldo() < MEMBRESIA_N_HAMMAZON){
-      System.out.println("No cuentas con el saldo suficiente
-                          para ser usuario de Hammazon Prime, tu suscripción será suspendida.");
+      System.out.println("Lo sentimos " + cliente.getNombre() + ", no cuentas con el saldo suficiente para ser usuario" +
+                         " de Hammazon Prime.");
       eliminarSuscriptor(cliente);
 
-    }else if(tipoDeMembresia.equals("premium")){
-      if(cliente.getSaldo() < MEMBRESIA_P_TWITSH){
-        System.out.println("No cuentas con el saldo suficiente
-                            para ser usuario premium de Twitsh, puedes suscribirte
-                            a nuestra membresía normal por un costo de 6 diarios.");
-        eliminarSuscriptor(cliente);
-      }else{
-        cliente.setSaldo(cliente.getSaldo() - MEMBRESIA_P_TWITSH);
-        System.out.println("Hola " + cliente.getNombre() + ", hemos cobrado tu suscripcion de Twitsh.");
-      }
+    }else if(tipoDeMembresia.equals("Normal")){
+      cliente.setSaldo(cliente.getSaldo() - MEMBRESIA_N_HAMMAZON);
+      System.out.println("Hola " + cliente.getNombre() +
+                         ", hemos cobrado tu suscripcion de Hammazon Prime.");
 
-    }else(tipoDeMembresia.equals("normal")){
-      cliente.getSaldo() = cliente.getSaldo() - MEMBRESIA_N_TWITSH;
-      System.out.println("Hola " + cliente.getNombre() + ", hemos cobrado tu suscripcion de Twitsh.");
+    }else if(cliente.getSaldo() < MEMBRESIA_P_HAMMAZON && tipoDeMembresia.equals("Premium")){
+      cliente.setSaldo(cliente.getSaldo() - MEMBRESIA_P_HAMMAZON);
+      System.out.println("Hola " + cliente.getNombre() +
+                         ", hemos cobrado tu suscripcion de Hammazon Prime.");
+
+    }else{
+      System.out.println("Tu saldo es insuficiente para la suscripcion de Hammazon Prime.");
+      eliminarSuscriptor(cliente);
     }
-
+  }
 }
