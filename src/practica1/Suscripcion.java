cf4ecbf6c0ef1e27b6770eec97230fb0c20d8e85
garayfernandor <72635259+garayfernandor@users.noreply.github.com>
2020-10-09 19:46:25 -0500

    Update Suscripcion.java

--- src/practica1/Suscripcion.java
+++ src/practica1/Suscripcion.java
@@ -1,75 +1,85 @@
 package myp.practica1;
 
 /**
- * Clase abstarcta de la suscripción que necesita el cliente.
+ * Clase abstarcta del Servicio que necesita el cliente.
  */
-public abstract class Suscripcion {
+public abstract class Servicio {
 
     private String tipoDeMembresia;
     private int costo;
-    private ArrayList<Cliente> usuarios;
+    private ArrayList<Cliente> clientes;
+    private String recomendacionDelDia;
 
 
     /**
-     * Constructor de Suscripcion
+     * Constructor de Servicio.
      */
-    public Suscripcion(String tipoDeMembresia, int costo){
+    public Servicio(String tipoDeMembresia, int costo){
       this.tipoDeMembresia = tipoDeMembresia;
       this.costo = costo;
     }
 
 
     /**
-     * Regresa el costo de la suscripcion
+     * Regresa el costo del servicio.
      */
     public int getCosto(){
       return costo;
     }
 
     /**
-     * Deine el costo de la suscripcion
+     * Deine el costo del servicio.
      */
      public void setCosto(int costo){
        this.costo = costo;
      }
     /**
-     * Regresa el tipo Tipo de Membresia de la suscripcion
+     * Regresa el tipo Tipo de Membresia del servicio.
      */
-    public String gettipoDeMembresia(){
+    public String getTipoDeMembresia(){
       return tipoDeMembresia;
     }
 
     /**
-     * Define el Tipo de Membresia de la suscripcion
+     * Define el Tipo de Membresia del servicio.
      */
-    public void settipoDeMembresia(String estado) {
+    public void setTipoDeMembresia(String tipoDeMembresia) {
         this.tipoDeMembresia = tipoDeMembresia;
     }
 
     /**
-     *  Agrega un cliente en la lista de usuarios de la suscripcion, de igual forma
-     *  agrega una suscripcion a la lista de suscripciones del cliente.
+     *  Agrega un cliente en la lista de clientes del servicio, de igual forma
+     *  agrega un servicio a la lista de servicios del cliente.
      */
-    public void agregaSuscriptor(Cliente cliente){
-      usuarios.add(cliente);
-      cliente.getSuscripciones().add(this);
+    public void agregaCliente(Cliente cliente){
+      clientes.add(cliente);
+      cliente.getServicios().add(this);
       System.out.println("Hola " + cliente.getNombre() +
-                         "te damos la bienvenida a " + getSuscripcion());
+                         ", te damos la bienvenida a " + getTipoDeMembresia());
     }
 
     /**
-     *  Elimina un cliente en la lista de usuarios de la suscripcion, de igual forma
-     *  elimina una suscripcion a la lista de suscripciones del cliente.
+     *  Elimina un cliente de la lista de clientes del servicio, de igual forma
+     *  elimina un servicio de la lista de servicios del cliente.
      */
-     public void eliminarSuscriptor(Cliente cliente){
-       if(!(usuarios.contains(cliente)))
-        return;
-       usuarios.remove(cliente);
-       cliente.getSuscripciones().remove(this);
+    public void eliminaCliente(Cliente cliente) throws NoSuchElementException {
+       if(!(clientes.contains(cliente)))
+           throw new NoSuchElementException();
+       clientes.remove(cliente);
+       cliente.getServicios().remove(this);
        System.out.println(cliente.getNombre() +   ", lamentamos" +
                           " que hayas abandonado tu suscripcion a "    +
-                          getSuscripcion());
-     }
+                          getTipoDeMembresia());
+    }
+    
+    /**
+     * Recomienda un producto del servicio al cliente, agregando dicho producto
+     * a su lista de recomendaciones.
+     */
+    public void recomiendaA(Cliente cliente) {
+        cliente.getRecomendaciones().add(recomendacionDelDia);
+        System.out.println("Hola " + cliente.getNombre() + "! de parte                                     de " toString() " y con base a tus gustos                                       queremos recomendarte " + recomendacionDelDia)	
+    }    
 
     public abstract String toString();
 }
